f7c7cafad69a857dcd0dba3be79d34a136af69d3
juanzhi0322 <54821571+juanzhi0322@users.noreply.github.com>
2022-01-22 17:22:38 -0500

    Add html and css

---
+++ script.js
@@ -0,0 +1,12 @@
+// modal
+const BASE_URL = "https://restcountries.com/v2/";
+const DATA = { countries: [], neighbors: [] };
+
+// view
+
+// controller
+const searchBtn = document.querySelector(".btn");
+function clickSearch() {
+  console.log("click run");
+}
+searchBtn.addEventListener("click", clickSearch);
