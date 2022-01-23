a67cffe6a84536bd792379001870158f04620d2c
juanzhi0322 <54821571+juanzhi0322@users.noreply.github.com>
2022-01-22 22:50:33 -0500

    add js

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const BASE_URL = "https://restcountries.com/v2/";
 const DATA = { countries: [], neighbors: [] };
 
 // view
-function createNewCard(oneCountry) {
+function createNewCard(oneCountry, isNeighbor) {
   const cardContainer = document.createElement("div");
   cardContainer.className = "country";
+  if (!isNeighbor) {
+    cardContainer.id = oneCountry.name;
+  }
   cardContainer.innerHTML = `
   <img class="country__img" src="${oneCountry.flag}" />
   <div class="country__data">
@@ -18,18 +21,43 @@ function createNewCard(oneCountry) {
   `;
   return cardContainer;
 }
-function updateView() {
+
+function updateCountries() {
   const countriesContainer = document.querySelector(".countries");
   countriesContainer.innerHTML = "";
-  DATA.countries.forEach((each) => {
-    const newCard = createNewCard(each);
-    countriesContainer.append(newCard);
-  });
+  const countriesContainerTitle = document.querySelector(".countries-title");
+  if (DATA.countries.length > 0) {
+    countriesContainerTitle.className = "countries-title show";
+    DATA.countries.forEach((each) => {
+      const newCard = createNewCard(each, false);
+      countriesContainer.append(newCard);
+    });
+  } else {
+    countriesContainerTitle.className = "countries-title";
+  }
+}
+
+function updateNeighbors() {
+  const neighborsContainer = document.querySelector(".neighbors");
+  neighborsContainer.innerHTML = "";
+  const neighborsContainerTitle = document.querySelector(".neighbors-title");
+  if (DATA.neighbors.length > 0) {
+    neighborsContainerTitle.className = "neighbors-title show";
+    DATA.neighbors.forEach((each) => {
+      const newNeighbors = createNewCard(each, true);
+      neighborsContainer.append(newNeighbors);
+    });
+  } else {
+    neighborsContainerTitle.className = "neighbors-title";
+  }
 }
 
 // controller
 const searchBtn = document.querySelector(".btn");
 const searchInput = document.querySelector(".input");
+
+const cardClickBtn = document.querySelector(".countries");
+
 function clickSearch() {
   const searchVal = searchInput.value;
   searchInput.value = "";
@@ -37,7 +65,38 @@ function clickSearch() {
     .then((res) => res.json())
     .then((data) => {
       DATA.countries = data;
-      updateView();
+      DATA.neighbors = [];
+      updateCountries();
+      updateNeighbors();
     });
 }
+
+function clickCard(event) {
+  const eventParent = event.target.closest(".country");
+  if (eventParent) {
+    const clickedCountry = eventParent.id;
+    const [filteredCountry] = DATA.countries.filter((each) => {
+      return each.name === clickedCountry;
+    });
+    if (filteredCountry.borders) {
+      const clickedNeighbors = filteredCountry.borders;
+      let formatNeighbors = "";
+      clickedNeighbors.forEach((each) => {
+        formatNeighbors += `${each},`;
+      });
+      fetch(`${BASE_URL}alpha?codes=${formatNeighbors}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          DATA.neighbors = data;
+          updateNeighbors();
+        });
+    } else {
+      DATA.neighbors = [];
+      updateNeighbors();
+    }
+  }
+}
 searchBtn.addEventListener("click", clickSearch);
+
+cardClickBtn.addEventListener("click", clickCard);
